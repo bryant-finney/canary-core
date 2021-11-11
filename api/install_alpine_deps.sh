47274f98db45713b77691bbdbabe8ff48d75ebcd
Bryant Finney <finneybp@gmail.com>
2021-11-11 15:12:10 -0500

    [#1] tabs -> spaces
    Signed-off-by: Bryant Finney <finneybp@gmail.com>

--- api/install_alpine_deps.sh
+++ api/install_alpine_deps.sh
@@ -14,37 +14,37 @@
 # -------------------------------------------------------------------------------------
 
 parse_args() {
-	# Parse the arguments, setting environment variables.
-	#
-	#   See also: function show_help()
-	#
-	#   Args:
-	#       $@                      the arguments to parse
-	#
-	#   Returns:
-	#       $DEV
+    # Parse the arguments, setting environment variables.
+    #
+    #   See also: function show_help()
+    #
+    #   Args:
+    #       $@                      the arguments to parse
+    #
+    #   Returns:
+    #       $DEV
 
-	while test $# -gt 0; do
-		case "$1" in
-		-e | --extras)
-			export INSTALL_EXTRAS=true
-			shift 1
-			;;
+    while test $# -gt 0; do
+        case "$1" in
+        -e | --extras)
+            export INSTALL_EXTRAS=true
+            shift 1
+            ;;
 
-		*)
-			error "Unrecognized argument: $1"
-			exit 1
-			;;
-		esac
-	done
+        *)
+            error "Unrecognized argument: $1"
+            exit 1
+            ;;
+        esac
+    done
 }
 
 parse_args "$@"
 
 apk add git gcc build-base linux-headers libxslt-dev libffi-dev openssl-dev \
-	musl-dev cargo rust postgresql-dev postgresql-client ttf-freefont
+    musl-dev cargo rust postgresql-dev postgresql-client ttf-freefont
 
 # this package is required for `pyreverse`, `pydeps`, and other `dev` dependencies
 if [ "$INSTALL_EXTRAS" = "true" ]; then
-	apk add graphviz-dev zsh neovim jq gnupg git neovim curl
+    apk add graphviz-dev zsh neovim jq gnupg git neovim curl
 fi
