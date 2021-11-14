118162566d2077cc59b8d44eae8ea0d18414c99e
Bryant Finney <finneybp@gmail.com>
2021-11-13 21:44:46 -0500

    [#3] promote some fixtures from `test_basic_api_client` to `conftest`
    Signed-off-by: Bryant Finney <finneybp@gmail.com>

--- canary_core/hc_api_connector/tests/conftest.py
+++ canary_core/hc_api_connector/tests/conftest.py
@@ -12,6 +12,8 @@ from pytest_django.fixtures import SettingsWrapper
 
 logger = logging.getLogger(__name__)
 
+pytestmark = pytest.mark.django_db
+
 
 @pytest.fixture
 def root_urlconf(settings: SettingsWrapper) -> SettingsWrapper:
@@ -27,3 +29,16 @@ def root_urlconf(settings: SettingsWrapper) -> SettingsWrapper:
     """
     settings.ROOT_URLCONF = __name__.rsplit(".", maxsplit=1)[0] + ".mock_api"
     return settings
+
+
+@pytest.fixture
+def enable_auth_class(settings: SettingsWrapper) -> None:
+    """Enable the mock API's auth class.
+
+    Args:
+        settings (SettingsWrapper): use this fixture to append the auth class to the
+            setting in ``REST_FRAMEWORK``
+    """
+    auth_class = "canary_core.hc_api_connector.tests.mock_auth.GenericAPIAuthentication"
+    if auth_class not in settings.REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"]:
+        settings.REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"].append(auth_class)
--- canary_core/hc_api_connector/tests/test_basic_api_client.py
+++ canary_core/hc_api_connector/tests/test_basic_api_client.py
@@ -12,7 +12,6 @@ from django.test.client import Client
 
 # third party
 import pytest
-from pytest_django.fixtures import SettingsWrapper
 from pytest_django.live_server_helper import LiveServer
 
 # local
@@ -27,21 +26,6 @@ if TYPE_CHECKING:
     from django.contrib.auth.models import User  # noqa: I005  # pragma: no cover
 else:
     User = get_user_model()
-
-pytestmark = pytest.mark.django_db
-
-
-@pytest.fixture
-def enable_auth_class(settings: SettingsWrapper) -> None:
-    """Enable the mock API's auth class.
-
-    Args:
-        settings (SettingsWrapper): use this fixture to append the auth class to the
-            setting in ``REST_FRAMEWORK``
-    """
-    auth_class = "canary_core.hc_api_connector.tests.mock_auth.GenericAPIAuthentication"
-    if auth_class not in settings.REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"]:
-        settings.REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"].append(auth_class)
 
 
 @pytest.fixture
